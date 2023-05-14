1e74148b1d0bb6797b1c972504c76cf12f965c50
jor-el <jerraill.d.murphy@gmail.com>
2023-05-14 22:55:26 +0200

    hack_or_snooze dirc

--- Axios/script.js
+++ Axios/script.js
@@ -1,17 +1,16 @@
-
 // ! Star Wars Data API
-// async function getData() {
-//   const response = await axios.get('https://swapi.dev/api/planets/');
-//   const { next: secondPage, results: firstSetOfResults } = response.data;
-//   const listOfPlanets = document.querySelector('#planets-list')
+async function getData() {
+  const response = await axios.get('https://swapi.dev/api/planets/');
+  const { next: secondPage, results: firstSetOfResults } = response.data;
+  const listOfPlanets = document.querySelector('#planets-list');
 
-//   firstSetOfResults
-//     .map((planet) => planet.name)
-//     .forEach((planet) => {
-//       const newLI = document.createElement('LI');
-//       newLI.innerText = planet;
-//       listOfPlanets.append(newLI);
-//     });
-// }
+  firstSetOfResults
+    .map((planet) => planet.name)
+    .forEach((planet) => {
+      const newLI = document.createElement('LI');
+      newLI.innerText = planet;
+      listOfPlanets.append(newLI);
+    });
+}
 
-// getData();
+getData();
