033fd670e949f8b95685bb46aa76a99cad4d6c99
Jerraill Murphy <Jerrraill.D.Murphy@gmail.com>
2023-05-15 14:23:11 +0200

    Buttons modified ; css added

--- AJAX Giphy Party/app.js
+++ AJAX Giphy Party/app.js
@@ -2,9 +2,9 @@ console.log("Let's get this party started!");
 
 $(document).ready(function() {
     $('#search-form').submit(function(event) {
-      event.preventDefault(); // Prevent form submission
+      event.preventDefault();
 
-      var searchTerm = $('#search-input').val();
+      let searchTerm = $('#search-input').val();
       console.log(searchTerm);
     });
   });
