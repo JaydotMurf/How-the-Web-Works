0fb210064e52415cfd28837554031529d1c41878
Jerraill Murphy <Jerrraill.D.Murphy@gmail.com>
2023-05-15 13:37:48 +0200

    Added Color Palette ; more styling

--- AJAX Giphy Party/app.js
+++ AJAX Giphy Party/app.js
@@ -5,26 +5,6 @@ $(document).ready(function() {
       event.preventDefault(); // Prevent form submission
 
       var searchTerm = $('#search-input').val();
-
-      // Make AJAX request to Giphy API
-      $.ajax({
-        url: 'https://api.giphy.com/v1/gifs/random',
-        type: 'GET',
-        dataType: 'json',
-        data: {
-          api_key: 'YOUR_API_KEY', // Replace with your Giphy API key
-          tag: searchTerm,
-          rating: 'pg-13' // You can change the rating if desired
-        },
-        success: function(response) {
-          var gifUrl = response.data.image_url;
-
-          // Display the GIF
-          $('#gif-container').html('<img src="' + gifUrl + '" alt="GIF">');
-        },
-        error: function() {
-          $('#gif-container').html('<p>Failed to load GIF.</p>');
-        }
-      });
+      console.log(searchTerm);
     });
   });
