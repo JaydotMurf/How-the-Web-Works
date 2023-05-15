f68fa9335849882021eca58436c1326b2a47d9b1
jor-el <jerraill.d.murphy@gmail.com>
2023-05-15 23:34:56 +0200

    append GIFs ; first attempt

--- AJAX Giphy Party/app.js
+++ AJAX Giphy Party/app.js
@@ -1,10 +1,27 @@
-console.log("Let's get this party started!");
+$(document).ready(function () {
+  console.log("Let's get this party started!");
 
-$(document).ready(function() {
-    $('#search-form').submit(function(event) {
-      event.preventDefault();
+  $(`#search-form`).on('submit', (e) => {
+    e.preventDefault();
 
-      let searchTerm = $('#search-input').val();
-      console.log(searchTerm);
-    });
+    const KEY = `DtY6Xwh7tRXaqDLWgk513pPmjmoBYRuP`;
+    let searchQuery = $(`#search-input`).val();
+    const amountOfGIFS = 1;
+
+    async function getGIF() {
+      let result = await axios.get(`http://api.giphy.com/v1/gifs/search`, {
+        params: { key: KEY, q: searchQuery, limit: amountOfGIFS },
+      });
+      const loopingGIF = result.data.data[0].images.fixed_width;
+      $(`#search-input`).val(``);
+      console.log(loopingGIF.mp4);
+
+      $('#gif-container').append(
+        `<div class="col gif"> <img src=${loopingGIF.url}> </div>`
+      );
+      return;
+    }
+
+    getGIF();
   });
+});
