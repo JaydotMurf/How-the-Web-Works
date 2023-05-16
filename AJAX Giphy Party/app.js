146514d3eee957395477e08e4a94d7379874de45
Jerraill Murphy <Jerrraill.D.Murphy@gmail.com>
2023-05-16 14:52:39 +0200

    submitted

--- AJAX Giphy Party/app.js
+++ AJAX Giphy Party/app.js
@@ -12,16 +12,22 @@ $(document).ready(function () {
       let result = await axios.get(`http://api.giphy.com/v1/gifs/search`, {
         params: { key: KEY, q: searchQuery, limit: amountOfGIFS },
       });
-      const loopingGIF = result.data.data[0].images.fixed_height;
+      const GIF = result.data.data[0].images.fixed_height;
       $(`#search-input`).val(``);
-      console.log(loopingGIF.mp4);
+      console.log(GIF.mp4);
 
       $('#image-container').append(
-        `<div class="col gif mb-2 offset-md-2"> <img src=${loopingGIF.url} class="rounded"> </div>`
+        `<div class="col col-lg-2 m-2 gif"> <img src=${GIF.url} class="rounded"> </div>`
       );
       return;
     }
-
     getGIF();
   });
+
+  $(`#remove-all`).on(`click`, (e) => {
+    e.preventDefault();
+
+    $(`.gif`).remove(); 
+
+  })
 });
